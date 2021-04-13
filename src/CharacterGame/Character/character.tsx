20010f521456abfebef9d5c4aa2888bfdaf10e53
jiwon <jidole041214@naver.com>
2021-04-13 14:38:36 +0900

    FIX : CHARACTER TAIL

--- src/CharacterGame/Character/character.tsx
+++ src/CharacterGame/Character/character.tsx
@@ -5,6 +5,7 @@ export const Character: FC = (): ReactElement => {
     ctx: CanvasRenderingContext2D;
     squerSize: number;
     margin: number;
+    // 클래스 생성시 초기화
     constructor() {
       this.squerSize = 5;
       this.margin = 100;
@@ -14,6 +15,8 @@ export const Character: FC = (): ReactElement => {
   const canvas = useRef();
   const cv: HTMLCanvasElement = canvas.current;
   const [load, setload] = useState<boolean>(false);
+
+  /* 라인 그리기 */
   function Line(x: number, y: number, cnt: number) {
     for (let i = 0; i < cnt; i++) {
       if (cv !== undefined) {
@@ -23,11 +26,13 @@ export const Character: FC = (): ReactElement => {
           c.squerSize * 4,
           c.squerSize,
           c.squerSize
-        );
+        ); // 캐릭터 눈알
         c.ctx.fillRect(c.margin + x + i, y, c.squerSize, c.squerSize);
       }
     }
   }
+
+  /* 몸통 그려주고 */
   function BodyDraw() {
     const size = c.squerSize;
     if(c.ctx !== undefined) c.ctx.clearRect(0, 0, cv.width, cv.height);
@@ -41,13 +46,28 @@ export const Character: FC = (): ReactElement => {
     Line(-3, size * 11, size * 4);
     Line(-8, size * 12, size * 5);
     Line(-13, size * 12, size * 8);
+    Line(26, size * 13, size/6);
+    /* 꼬리쪽 */
+    Line(-43, size * 10, size /3);
+    Line(-43, size * 11, size * 1);
+    Line(-43, size * 12, size * 2);
+    Line(-43, size * 13, size * 5);
+    Line(-43, size * 14, size * 4);
+    Line(-38, size * 15, size * 4);
+    Line(-33, size * 16, size * 4);
+    Line(-28, size * 17, size * 4);
+    Line(-23, size * 18, size * 4);
+    /*  */
     Line(-18, size * 13, size * 7);
     Line(-23, size * 14, size * 8);
     Line(-28, size * 15, size * 9);
     Line(-23, size * 16, size * 7);
     Line(-18, size * 17, size * 6);
     Line(-13, size * 18, size * 5);
+    Line(-18, size * 19, size * 5.1);
   }
+
+  /* 다리 왔다리 갔다리 */
   function draw(): void {
     const size = c.squerSize;
     let i = 0;
@@ -55,26 +75,30 @@ export const Character: FC = (): ReactElement => {
       i++;
       if (i % 2 == 0) {
         BodyDraw();
-        Line(-13, size * 19, size);
-        Line(7, size * 19, size);
-        Line(7, size * 20, size);
-        Line(7, size * 21, size);
-        Line(7, size * 22, size * 2);
-        Line(-8, size * 20, size * 1);
+        Line(-18, size * 20, size);
+        Line(2, size * 20, size);
+        Line(2, size * 21, size);
+        Line(2, size * 22, size);
+        Line(2, size * 23, size * 2);
+        Line(-13, size * 21, size * 1);
       } else {
         BodyDraw();
-        Line(8, size * 19, size);
-        Line(-8, size * 19, size);
-        Line(-8, size * 20, size);
-        Line(-8, size * 21, size);
-        Line(-8, size * 22, size * 2);
-        Line(10, size * 20, size * 1);
+        Line(3, size * 20, size);
+        Line(-13, size * 20, size);
+        Line(-13, size * 21, size);
+        Line(-13, size * 22, size);
+        Line(-13, size * 23, size * 2);
+        Line(5, size * 21, size * 1);
       }
     }, 100);
   }
+
+  
   useEffect(() => {
     draw();
   }, [load]);
+  /* 첨에 실행 */
+
   useEffect(() => {
     setload(!load); // 캔버스 한번 리렌더링 해줘야됨
   }, []);
@@ -82,7 +106,7 @@ export const Character: FC = (): ReactElement => {
     <>
       <canvas
         ref={canvas}
-        style={{ marginTop: "100px", marginLeft: "100px" }}
+        style={{ marginTop: "10%", marginLeft: "20%" }}
       />
     </>
   );
